9109728e98d5daa81ff5b106a45cb2544d713333
Unknown <yzj999@gmail.com>
2019-02-11 16:18:46 -0500

    Quick fix on parser
    ToDo: need to use array list to store all data

--- TreeWithoutMaven/src/parser.java
+++ TreeWithoutMaven/src/parser.java
@@ -10,6 +10,7 @@ import java.util.*;
 
 public class parser{
 	
+	// HashMap won't work because x-values are not unique.
 	static HashMap<Double, Double> dataset1 = new HashMap<Double, Double>();
 	static HashMap<ArrayList<Double>, Double> dataset2 = new HashMap<ArrayList<Double>, Double>();
 	static HashMap<Double, Double> dataset3 = new HashMap<Double, Double>();
@@ -63,34 +64,32 @@ public class parser{
 			Double x1 = Double.valueOf(fields[0]);
 			Double y = Double.valueOf(fields[1]); 
 			dataset1.put(x1, y);
-			System.out.println(dataset1.get(x1));
 		} 
 		
 		br1.close();
 		
-//		// Parser Dataset2
-//		BufferedReader br2 = new BufferedReader(new FileReader("dataset1.csv")); 
-//		String line2 = br2.readLine(); // Reading header, Ignoring 
-//		ArrayList<Double> data = new ArrayList<Double>(3);
-//		
-//		while ((line2 = br2.readLine()) != null && !line2.isEmpty()) {
-//			String[] fields = line2.split(","); 
-//			System.out.println(fields[0] + fields[1] + fields[2] + fields[3]);
-//			Double x1 = Double.valueOf(fields[0]); 
-//			Double x2 = Double.valueOf(fields[1]); 
-//			Double x3 = Double.valueOf(fields[2]);
-////			
-//			Double y = Double.valueOf(fields[3]);
-//			data.add(0, x1);
-//			data.add(1, x2);
-//			data.add(2, x3);
-//			dataset2.put(data, y);
-//		} 
-//		
-//		br2.close(); 
+		// Parser Dataset2
+		BufferedReader br2 = new BufferedReader(new FileReader("dataset2.csv")); 
+		String line2 = br2.readLine(); // Reading header, Ignoring 
+		ArrayList<Double> data = new ArrayList<Double>(3);
+		
+		while ((line2 = br2.readLine()) != null && !line2.isEmpty()) {
+			String[] fields = line2.split(","); 
+			
+			Double x1 = Double.valueOf(fields[0]); 
+			Double x2 = Double.valueOf(fields[1]); 
+			Double x3 = Double.valueOf(fields[2]);	
+			Double y = Double.valueOf(fields[3]);
+			data.add(0, x1);
+			data.add(1, x2);
+			data.add(2, x3);
+			dataset2.put(data, y);
+		} 
+		
+		br2.close(); 
 		
 		// Parser Dataset3
-		BufferedReader br3 = new BufferedReader(new FileReader("dataset1.csv")); 
+		BufferedReader br3 = new BufferedReader(new FileReader("dataset3.csv")); 
 		String line3 = br3.readLine(); // Reading header, Ignoring 
 		
 		while ((line3 = br3.readLine()) != null && !line3.isEmpty()) { 
